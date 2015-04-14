498445f79ff180fe35a4d992b9e629bb5b5270d0
bcorsak <bcorsak@229c8da2-4750-4889-ae1e-125d854c4395>
2015-04-14 10:09:24 +0000

    Case 191. Added production url.

--- src/main/java/com/digisigner/client/http/Config.java
+++ src/main/java/com/digisigner/client/http/Config.java
@@ -5,7 +5,7 @@ package com.digisigner.client.http;
  */
 public final class Config {
 
-    public static final String SERVER = "http://localhost:8080/online/api";
+    public static final String SERVER = "https://digisigner.com/online/api";
     public static final String VERSION = "/v1";
     public static final String SERVER_URL = SERVER + VERSION;
     public static final String DOCUMENTS_URL = SERVER_URL + "/documents";
